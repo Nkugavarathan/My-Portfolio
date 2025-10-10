ed72f252c9c1fcd14100aae0f101556e0fdd7941
Nkugavarathan <kugavarathan28@gmail.com>
2025-10-10 15:39:42 +0530

    resume updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import Container from "react-bootstrap/Container"
-import logo from "../Assets/logo.png"
+// import logo from "../Assets/logo.png"
 // import Button from "react-bootstrap/Button"
 import { Link } from "react-router-dom"
 
@@ -36,8 +36,8 @@ function NavBar() {
       className={navColour ? "sticky" : "navbar"}
     >
       <Container>
-        <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+        <Navbar.Brand href="/" className="d-flex ">
+          <h3>PORTFOLIO</h3>
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import React, { useState, useEffect } from "react"
+import { Container, Row } from "react-bootstrap"
+import Button from "react-bootstrap/Button"
+import Particle from "../Particle"
+import pdf from "../../Assets/../Assets/kugavarathan.pdf"
+import { AiOutlineDownload } from "react-icons/ai"
+import { Document, Page, pdfjs } from "react-pdf"
+import "react-pdf/dist/esm/Page/AnnotationLayer.css"
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+  const [width, setWidth] = useState(1200)
 
   useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
+    setWidth(window.innerWidth)
+  }, [])
 
   return (
     <div>
@@ -50,7 +50,7 @@ function ResumeNew() {
         </Row>
       </Container>
     </div>
-  );
+  )
 }
 
-export default ResumeNew;
+export default ResumeNew
