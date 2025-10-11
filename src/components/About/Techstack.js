6182d50726791af3ee07a3793129e07fbc2715e5
Nkugavarathan <kugavarathan28@gmail.com>
2025-10-11 11:39:26 +0530

    change techsack freame motion

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -22,55 +22,48 @@ import {
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <motion.div
-        initial={{ opacity: 0, y: -100 }}
-        whileInView={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
-        viewport={{ once: false, amount: 0.3 }}
-      >
-        <Col xs={4} md={2} className="tech-icons">
-          <DiJava />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiJavascript1 />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiPhp />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiHtml5 />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiCss3 />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiReact />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <SiNextdotjs />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <SiExpress />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiNodejs />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <SiSpringboot />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiMongodb />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <SiTailwindcss />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <SiBootstrap />
-        </Col>
-        <Col xs={4} md={2} className="tech-icons">
-          <DiGit />
-        </Col>
-      </motion.div>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPhp />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNextdotjs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiExpress />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSpringboot />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTailwindcss />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiBootstrap />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
     </Row>
   )
 }
